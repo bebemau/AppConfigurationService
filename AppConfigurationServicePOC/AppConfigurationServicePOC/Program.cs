f213d39eae414725fbb28b26ea55818fb7e681a8
kman <kman@L-QW1489>
2020-04-08 17:11:46 -0700

    push all files

---
+++ AppConfigurationServicePOC/AppConfigurationServicePOC/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Azure.Identity;
+using System.IO;
+
+namespace AppConfigurationServicePOC
+{
+    class Program
+    {
+        private static IConfigurationRoot Configuration;
+
+        static void Main()
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+
+            var appConfigServiceConnection = Configuration["AppConfigServiceEndpoint:Endpoint"];
+            builder.AddAzureAppConfiguration(options => options.ConnectWithManagedIdentity(appConfigServiceConnection));
+
+            Configuration = builder.Build();
+            Console.WriteLine(Configuration["MyPassword"]);
+            Console.ReadLine();
+        }
+    }
+}
