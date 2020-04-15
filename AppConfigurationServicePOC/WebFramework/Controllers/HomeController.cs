6cd7f214236308b3b8d104024276b1973b97941b
kman <kman@L-QW1489>
2020-04-14 19:34:29 -0700

    web framework working with conn string

---
+++ AppConfigurationServicePOC/WebFramework/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var setting = ConfigurationManager.AppSettings["MyPassword"];
+
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
